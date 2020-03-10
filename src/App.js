71aea34511c0495afb1c61feaaef283d10a4c6a7
Hasan71 <mahmud.cse7@gmail.com>
2020-03-11 02:33:27 +0600

    adding initial codes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name  = "Mahfuz";
+
+  var person = {
+    name: "Doktor Mahfuz",
+    job: "Singer"
+  }
+
+  var person2 = {
+    name: "Eva Rahman",
+    job: "Kokil konthi"
+  }
+
+  var result = [];
+   for(let i=0; i<10; i++){
+     var result2 = result.push(i);
+  }
+
+  var mystyle = {
+    color: 'orange',
+    backgroundColor: 'cyan'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +32,19 @@ function App() {
         <p>
           Edit Done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          // className="App-link"
-          // href="https://reactjs.org"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
+        <a>
           Learn React
         </a>
+        <h1>My heading {2+5*25+65}</h1>
+        <h2 style={{backgroundColor:'green', color: 'blue'}}>This is Doctor {name}</h2>
+        <h3>{result}</h3>
+        <p>My first react paragraph</p>
+        <p>oh my my {person.job + " " + person.name} </p>
+
+        <h3>She is {person2.name} and her job is {person2.job} </h3>
+
+        <h4 style={mystyle} >Singer: {person2.name + " Job: " + person2.job} </h4>
+
       </header>
     </div>
   );
